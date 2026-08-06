7c545668b5ff59775601a058138c4a3def3dcf16
scrumteam01 <sergiogarzon312@gmail.com>
2026-08-05 21:38:13 -0500

    Spring Boot

---
+++ paguemos/src/main/java/paguemos/paguemos/PaguemosApplication.java
@@ -0,0 +1,13 @@
+package paguemos.paguemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaguemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaguemosApplication.class, args);
+	}
+
+}
---
+++ paguemos/src/test/java/paguemos/paguemos/PaguemosApplicationTests.java
@@ -0,0 +1,13 @@
+package paguemos.paguemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaguemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
